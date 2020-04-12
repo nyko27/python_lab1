d33d35cad61bc3cca68d747199b8cc5ee94c1b78
Yuriy Nykolyshak <yuranykolysak@gmail.com>
2020-04-12 20:30:13 +0300

    fixed lab1

--- ladder.py
+++ ladder.py
@@ -5,7 +5,7 @@ class Ladder():
 	def __init__(self, producer = "Intertool", max_length_in_metres = 5, max_load = 150, width = 40, mass_in_kilograms = 4,
 	 material = "aluminum", warranty_in_years = 7):
 		self.producer = producer
-		self.max_length_in_metresc = max_length_in_metres
+		self.max_length_in_metres = max_length_in_metres
 		self.max_load = max_load
 		self.material = material
 		self.width = width
@@ -17,7 +17,7 @@ class Ladder():
 		"ladder deleted"
 
 	def __str__(self):
-		return 	self.producer + ',' + str(self.max_length_in_metresc) + ',' + str(self.max_load) + ',' + self.material + ',' + str(self.width) + ',' + str(self.mass_in_kilograms) + ',' + str(self.warranty_in_years)
+		return 	self.producer + ',' + str(self.max_length_in_metres) + ',' + str(self.max_load) + ',' + self.material + ',' + str(self.width) + ',' + str(self.mass_in_kilograms) + ',' + str(self.warranty_in_years)
 		  
 	@staticmethod
 	def get_amount_of_ladders():
@@ -33,5 +33,5 @@ def main():
 	print('Second ladder:', second_ladder)
 	print('Third ladder:', third_ladder)
 	print('\nAmount of ladders:', Ladder.get_amount_of_ladders() )
-	
+
 main()	
